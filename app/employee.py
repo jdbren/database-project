9d18a2b03cd48f65cb7218164987abdd69c2852a
jdb360 <jdb360@georgetown.edu>
2024-11-30 17:47:34 -0500

    updates to work with new schema

--- app/employee.py
+++ app/employee.py
@@ -33,7 +33,7 @@ def insert():
             if request.form['degree']:
                 degree = request.form['degree']
             experience = request.form['experience']
-            # PositionsHistory data
+            # EmployeePositions data
             position = request.form['position']
             employment_type = request.form['employment_type']
             salary = request.form['salary']
@@ -42,7 +42,7 @@ def insert():
             if health_insurance == 'company':
                 health_insurance_start_date = datetime.date(datetime.now())
             external_hire = request.form['external_hire']
-            # DepartmentsHistory data
+            # EmployeeDepartments data
             selected_departments = request.form.getlist('departments')
             # BenefitsHistory data
             selected_benefits = request.form.getlist('benefits')
@@ -58,8 +58,7 @@ def insert():
                     ExternalYearsWorked
                 ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
             ''', (ssn, fname, lname, gender, dob, address, city, state, postcode,
-                phone, degree, experience)
-            )
+                phone, degree, experience))
 
             id = cursor.lastrowid
 
@@ -100,17 +99,17 @@ def insert():
             flash('An error occurred while adding the employee')
             return "Error", HTTPStatus.INTERNAL_SERVER_ERROR
 
-    gendersList = search_db('SELECT Name FROM Genders', cursors.DictCursor)
-    degreesList = search_db('SELECT Name FROM Degrees', cursors.DictCursor)
-    benefitsList = search_db('SELECT Name FROM Benefits', cursors.DictCursor)
-    positionsList = search_db('SELECT Name FROM Positions', cursors.DictCursor)
-    departmentsList = search_db('SELECT Name FROM Departments', cursors.DictCursor)
+    genders_list = search_db('SELECT Name FROM Genders', cursors.DictCursor)
+    degrees_list = search_db('SELECT Name FROM Degrees', cursors.DictCursor)
+    benefits_list = search_db('SELECT Name FROM Benefits', cursors.DictCursor)
+    positions_list = search_db('SELECT Name FROM Positions', cursors.DictCursor)
+    departments_list = search_db('SELECT Name FROM Departments', cursors.DictCursor)
     return render_template('employee/form.html',
-        departments=departmentsList,
-        positions=positionsList,
-        benefits=benefitsList,
-        degrees=degreesList,
-        genders=gendersList
+        departments=departments_list,
+        positions=positions_list,
+        benefits=benefits_list,
+        degrees=degrees_list,
+        genders=genders_list
     )
 
 
@@ -151,11 +150,11 @@ def search():
             FROM
                 Employees AS s
             LEFT JOIN
-                DepartmentsHistory AS dh
-                ON s.ID = dh.ID AND dh.EndDate IS NULL
+                EmployeeDepartments AS dh
+                ON s.ID = dh.ID
             INNER JOIN
-                PositionsHistory AS ph
-                ON s.ID = ph.ID AND ph.EndDate IS NULL
+                EmployeePositions AS ph
+                ON s.ID = ph.ID
         """
 
         # Build WHERE conditions
@@ -261,13 +260,13 @@ def view(id):
             ph.Position,
             ph.Salary
         FROM
-            Staff AS s
+            Employees AS s
         LEFT JOIN
-            DepartmentsHistory AS dh
-            ON s.ID = dh.ID AND dh.EndDate IS NULL
+            EmployeeDepartments AS dh
+            ON s.ID = dh.ID
         INNER JOIN
-            PositionsHistory AS ph
-            ON s.ID = ph.ID AND ph.EndDate IS NULL
+            EmployeePositions AS ph
+            ON s.ID = ph.ID
         WHERE
             s.ID = %s
         GROUP BY
@@ -302,7 +301,7 @@ def edit(id):
             if request.form['degree']:
                 degree = request.form['degree']
             experience = request.form['experience']
-            # PositionsHistory data
+            # EmployeePositions data
             position = request.form['position']
             employment_type = request.form['employment_type']
             salary = request.form['salary']
@@ -311,9 +310,9 @@ def edit(id):
             if health_insurance == 'company':
                 health_insurance_start_date = datetime.date(datetime.now())
             external_hire = request.form['external_hire']
-            # DepartmentsHistory data
+            # EmployeeDepartments data
             selected_departments = request.form.getlist('departments')
-            # BenefitsHistory data
+            # Benefits data
             selected_benefits = request.form.getlist('benefits')
 
             db = open_db()
@@ -328,21 +327,19 @@ def edit(id):
                     ExternalYearsWorked = %s
                 WHERE ID = %s
             ''', (ssn, fname, lname, gender, dob, address, city, state, postcode,
-                phone, degree, experience, id)
-            )
+                phone, degree, experience, id))
 
             if (current_position['Position'] != position
             or current_position['Salary'] != salary
             or current_position['EmploymentType'] != employment_type):
-                # Update the old record, maybe make this trigger
+                # Remove records from active table
                 cursor.execute('''
-                    UPDATE PositionsHistory
-                    SET EndDate = CURDATE()
-                    WHERE ID = %s AND EndDate IS NULL
+                    DELETE FROM EmployeePositions
+                    WHERE ID = %s
                 ''', (id,))
                 # Insert updated information
                 cursor.execute('''
-                    INSERT INTO PositionsHistory (
+                    INSERT INTO EmployeePositions (
                         ID, StartDate, Position, EmploymentType, Salary,
                         IsExternalHire, HealthCoverageStartDate
                     ) VALUES (%s, CURDATE(), %s, %s, %s, 0, %s)
@@ -353,16 +350,15 @@ def edit(id):
             for department in current_departments:
                 if department not in selected_departments:
                     cursor.execute('''
-                        UPDATE DepartmentsHistory
-                        SET EndDate = CURDATE()
-                        WHERE ID = %s AND Department = %s AND EndDate IS NULL
+                        DELETE FROM EmployeeDepartments
+                        WHERE ID = %s AND Department = %s
                     ''', (id, department))
 
             # Add new departments
             for department in selected_departments:
                 if department not in current_departments:
                     cursor.execute('''
-                        INSERT INTO DepartmentsHistory (
+                        INSERT INTO EmployeeDepartments (
                             ID, Department, StartDate
                         ) VALUES (%s, %s, CURDATE())
                     ''', (id, department))
@@ -397,9 +393,11 @@ def edit(id):
 
 
     emp = search_db('SELECT * FROM Employees WHERE ID = %s', cursors.DictCursor, False, (id,))
-    benefitsList = search_db('SELECT Name FROM Benefits', cursors.DictCursor)
-    positionsList = search_db('SELECT Name FROM Positions', cursors.DictCursor)
-    departmentsList = search_db('SELECT Name FROM Departments', cursors.DictCursor)
+    benefits_list = search_db('SELECT Name FROM Benefits', cursors.DictCursor)
+    positions_list = search_db('SELECT Name FROM Positions', cursors.DictCursor)
+    departments_list = search_db('SELECT Name FROM Departments', cursors.DictCursor)
+    genders_list = search_db('SELECT Name FROM Genders', cursors.DictCursor)
+    degrees_list = search_db('SELECT Name FROM Degrees', cursors.DictCursor)
 
     emp['Departments'] = current_departments
     emp['Benefits'] = current_benefits
@@ -409,19 +407,28 @@ def edit(id):
         emp=emp,
         positions=positions_list,
         departments=departments_list,
-        benefits=benefits_list
+        benefits=benefits_list,
+        degrees=degrees_list,
+        genders=genders_list
     )
 
 @bp.delete('<int:id>')
-def delete(id):
+def archive_employee(id):
     try:
-        emp = search_db('SELECT ID, LastName FROM Staff WHERE ID = %s',
-            cursors.DictCursor, (id,))
+        emp = search_db('SELECT ID, LastName FROM Employees WHERE ID = %s',
+            cursors.DictCursor, False, (id,))
         if not emp:
             return "Employee not found", HTTPStatus.NOT_FOUND
-        execute_and_commit("DELETE FROM Staff WHERE ID = %s", (id,))
+        db = open_db()
+        cursor = db.cursor()
+        cursor.execute("DELETE FROM EmployeeDepartments WHERE ID = %s", (id,))
+        cursor.execute("DELETE FROM EmployeeBenefits WHERE ID = %s", (id,))
+        cursor.execute("DELETE FROM EmployeePositions WHERE ID = %s", (id,))
+        cursor.close()
+        db.commit()
+        close_db()
         # Rest of the data is deleted by cascade
-        flash(f"Employee {emp['ID']} deleted successfully")
+        flash(f"Employee {emp['ID']} archived successfully")
         return "Success", HTTPStatus.OK
     except Exception as e:
         print(e)
@@ -430,14 +437,14 @@ def delete(id):
 
 
 def get_employee_departments(id):
-    currentDepartments = search_db('''
-        SELECT Department FROM DepartmentsHistory WHERE ID = %s AND EndDate IS NULL
-    ''', cursors.Cursor, (id,))
+    current_departments = search_db('''
+        SELECT Department FROM EmployeeDepartments WHERE ID = %s
+    ''', cursors.Cursor, True, (id,))
     current_departments = [department[0] for department in current_departments]
     return current_departments
 
 def get_employee_benefits(id):
-    currentBenefits = search_db('''
+    current_benefits = search_db('''
         SELECT Benefit FROM EmployeeBenefits WHERE ID = %s AND EndDate IS NULL
     ''', cursors.Cursor, True, (id,))
     current_benefits = [benefit[0] for benefit in current_benefits]
@@ -445,5 +452,5 @@ def get_employee_benefits(id):
 
 def get_employee_position(id):
     return search_db('''
-        SELECT Salary, Position FROM PositionsHistory WHERE ID = %s AND EndDate IS NULL
-    ''', cursors.DictCursor, (id,), False)
+        SELECT Salary, Position FROM EmployeePositions WHERE ID = %s
+    ''', cursors.DictCursor, False, (id,))
