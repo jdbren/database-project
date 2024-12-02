4d2ba1c0ea465028648a93924de96213bddd80db
jdb360 <jdb360@georgetown.edu>
2024-12-01 23:02:56 -0500

    fixes to emp

--- app/employee.py
+++ app/employee.py
@@ -102,14 +102,15 @@ def insert():
     benefits_list = search_db('SELECT Name FROM Benefits', cursors.DictCursor)
     positions_list = search_db('SELECT Name FROM Positions', cursors.DictCursor)
     departments_list = search_db('SELECT Name FROM Departments', cursors.DictCursor)
+    employment_types = search_db('SELECT Name FROM EmploymentTypes', cursors.DictCursor)
     return render_template('employee/form.html',
         departments=departments_list,
         positions=positions_list,
         benefits=benefits_list,
         degrees=degrees_list,
-        genders=genders_list
+        genders=genders_list,
+        employment_types=employment_types
     )
-
 
 
 @bp.get('/search')
@@ -229,12 +230,14 @@ def search():
         departments_list = search_db('SELECT Name FROM Departments', cursors.DictCursor)
         genders_list = search_db('SELECT Name FROM Genders', cursors.DictCursor)
         degrees_list = search_db('SELECT Name FROM Degrees', cursors.DictCursor)
+        employment_types = search_db('SELECT Name FROM EmploymentTypes', cursors.DictCursor)
         return render_template('employee/search.html',
             employees=results,
             positions=positions_list,
             departments=departments_list,
             genders=genders_list,
-            degrees=degrees_list
+            degrees=degrees_list,
+            employment_types=employment_types
         )
     except Exception as e:
         print(e)
@@ -426,6 +429,7 @@ def archive_employee(id):
         cursor.execute("DELETE FROM EmployeeDepartments WHERE ID = %s", (id,))
         cursor.execute("DELETE FROM EmployeeBenefits WHERE ID = %s", (id,))
         cursor.execute("DELETE FROM EmployeePositions WHERE ID = %s", (id,))
+        cursor.execute("DELETE FROM EmployeeRoles WHERE EmployeeID = %s", (id,))
         cursor.close()
         db.commit()
         close_db()
