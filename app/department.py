ad9ac45b6862bf9536c04bc230c65dfe6cfba09c
jdb360 <jdb360@georgetown.edu>
2024-12-02 23:27:24 -0500

    add stats for departments

--- app/department.py
+++ app/department.py
@@ -4,13 +4,9 @@ from app.db import search_db
 
 bp = Blueprint('department', __name__, url_prefix='/department')
 
-@bp.get('/search')
-def search_department():
-    departments_list = search_db('SELECT Name FROM Departments', cursors.DictCursor)
-    selected_department = request.args.get('department')
-    if not selected_department:
-        return render_template('department/search.html', dept_list=departments_list, args=request.args)
-
+@bp.get('<string:department>')
+def department_info(department):
+    selected_department = department
     table = 'EmployeeDepartmentsHistory' if request.args.get('history') else 'EmployeeDepartments'
 
     query = "SELECT e.ID, CONCAT(e.FirstName, ' ', e.LastName) AS Name, ed.StartDate "
@@ -29,6 +25,29 @@ def search_department():
     print(query)
 
     results = search_db(query, cursors.DictCursor)
-    return render_template('department/search.html', dept_list=departments_list,
-                           args=request.args, dept=selected_department,
-                           employees=results)
+    return render_template('department/view.html',
+                           dept=selected_department,
+                           employees=results,
+                           history=bool(request.args.get('history')))
+
+@bp.get('search')
+def search_department():
+    dept_list = search_db('SELECT Name FROM Departments', cursors.DictCursor)
+    dept_table = 'EmployeeDepartmentsHistory' if request.args.get('history') else 'EmployeeDepartments'
+    pos_table = 'EmployeePositionsHistory' if request.args.get('history') else 'EmployeePositions'
+    order_by = request.args.get('order_by', 'Department')
+    asc = request.args.get('order', 'ASC')
+
+    query = f'''
+        SELECT ed.Department,
+            COUNT(ed.ID) AS NumEmployees,
+            ROUND(AVG(ep.Salary)) AS AvgSalary,
+            MIN(ed.StartDate) AS EarliestStartDate
+        FROM {dept_table} ed
+        JOIN {pos_table} ep ON ed.ID = ep.ID
+        GROUP BY ed.Department
+        ORDER BY {order_by} {asc}
+    '''
+
+    departments = search_db(query, cursors.DictCursor)
+    return render_template('department/search.html', dept_list=dept_list, departments=departments, args=request.args)
